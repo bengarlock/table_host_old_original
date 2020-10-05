81bb1c94f7cd07932d80bd8df2aaee1aef87d1f4
BenGarlock <ben.garlock@gmail.com>
2020-10-05 18:10:02 -0400

    testing ips

--- back_end/config/deploy/production.rb
+++ back_end/config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "192.168.86.45", user: "deploy", roles: %w{app db web}
+server "127.0.0.1", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
